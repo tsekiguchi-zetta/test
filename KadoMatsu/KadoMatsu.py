04d3f5915131698f5606a44e5e252836541fcd3e
tsekiguchi-zetta <tsekiguchi@zetta.co.jp>
2025-09-25 11:42:24 +0900

    引数刷新

--- KadoMatsu/KadoMatsu.py
+++ KadoMatsu/KadoMatsu.py
@@ -18,7 +18,7 @@ def cleanup():
 def init():
 	jpype.startJVM(jpype.getDefaultJVMPath(), '-Xmx48000m', '-XX:+UseG1GC', '-Djava.locale.providers=COMPAT,CLDR')
 
-	jar_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zadamexec.jar')
+	jar_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'KadoMatsu.jar')
 	jpype.addClassPath(jar_path)
 
 	global __kmClass
@@ -44,10 +44,26 @@ def setLog(
 	global __km
 	__km.initialise(version, command, information, error, warning, debug, timer)
 
-def createDM():
+def createDataMart(
+	*dmargs: str,
+	csv: str = None,
+	useItem: str = ...,
+	csv_type: str = "CSV1S",
+	csv_code: str = "SHIFT_JIS"):
 	global __km
-	dm = kmDM(__km.createDM());
-	return dm;
+	dm = __km.createDataMart()
+
+#	if useItem:
+#		dm.useItem(useItem)
+
+	if dmargs:
+		if dmargs[0]:
+			csv = dmargs[0]
+
+	dm.readCSV(csv, csv_type, csv_code)
+
+	kmDm = kmDM(dm);
+	return kmDm;
 
 class kmDM:
 
@@ -56,42 +72,25 @@ class kmDM:
 	def __init__(self, dm):
 		self._DM = dm
 
-	def item(self, item_list):
-		for item in item_list:
-			self._DM.defineItem(item[0], item[1], item[2])
-		return self
+	def select(self, *args: str):
+		print(">>KadoMatsu:DM:select ", args)
+		dm = self._DM.select(args)
+		kmDm = kmDM(dm);
+		return kmDm;
 
-	def addCalcItem(self, item_list):
-		for item in item_list:
-			self._DM.createCalcItem(item[0], item[1])
-		return self
+	def where(self, args: str):
+		print(">>KadoMatsu:DM:where ", args)
+		dm = self._DM.where(args)
+		kmDm = kmDM(dm);
+		return kmDm;
 
-	def readCSV(self, name):
-		self._DM.readCSV(name, "CSV1S", "SHIFT_JIS")
-		return self
+	def groupBy(self, *args: str):
+		print(">>KadoMatsu:DM:groupBy ", args)
+		dm = self._DM.groupBy(args)
+		kmDm = kmDM(dm);
+		return kmDm;
 
-	def createSummary(self):
-		sm = kmSM(self._DM.toSummary())
-		return sm
-
-class kmSM:
-
-	_SM = None
-
-	def __init__(self, sm):
-		self._SM = sm
-
-	def item(self, key_list, stat_list):
-		for key in key_list:
-			self._SM.defineKey(key)
-		for stat in stat_list:
-			self._SM.defineStat(stat[0], stat[1])
-		return self
-
-	def tabulate(self):
-		self._SM.tabulate()
-		return self
-
-	def toCSV(self, name):
-		self._SM.toCSV(name, "CSV1S", "SHIFT_JIS")
-		return self
+	def toCSV(self, *csv: str, 
+			csv_type: str = "CSV1S",
+			csv_code: str = "SHIFT_JIS"):
+		self._DM.toCSV(csv[0], csv_type, csv_code)
