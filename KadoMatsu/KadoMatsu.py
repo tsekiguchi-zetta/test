a3ddd5e301e78a13d15651ea548f4fb904de25d8
tsekiguchi-zetta <tsekiguchi@zetta.co.jp>
2025-09-25 13:03:42 +0900

    add setLog KadoMatsuInfo

--- KadoMatsu/KadoMatsu.py
+++ KadoMatsu/KadoMatsu.py
@@ -2,35 +2,26 @@ import jpype
 import os
 import atexit
 
-global __kmClass
-global __km
+__kmClass = None
+__kmIns = None
+__kmLogSW = False
 
 def cleanup():
-
-	global __km
-	__km.finalise()
-
+	global __kmIns
+	__kmIns.finalise()
 	jpype.shutdownJVM()
-
-	print('>>KadoMatsu::jpype.shutdownJVM')
-
 
 def init():
 	jpype.startJVM(jpype.getDefaultJVMPath(), '-Xmx48000m', '-XX:+UseG1GC', '-Djava.locale.providers=COMPAT,CLDR')
-
 	jar_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'KadoMatsu.jar')
 	jpype.addClassPath(jar_path)
-
 	global __kmClass
 	__kmClass = jpype.JClass('jp.co.zetta.zadam.zadamexec.ZAdam')
-
-	global __km
-	__km = __kmClass();
-
+	global __kmIns
+	__kmIns = __kmClass();
+	global __kmLogSW
+	__kmLogSW = False
 	atexit.register(cleanup)
-
-	print('>>KadoMatsu::jpype.startJVM')
-
 
 def setLog(
 	version: bool = False,
@@ -39,10 +30,13 @@ def setLog(
 	error: bool = True,
 	warning: bool = False,
 	debug: bool = False,
-	timer: bool = False
+	timer: bool = False,
+	KadoMatsuInfo: bool = False
 	) -> None:
-	global __km
-	__km.initialise(version, command, information, error, warning, debug, timer)
+	global __kmIns
+	__kmIns.initialise(version, command, information, error, warning, debug, timer)
+	global __kmLogSW
+	__kmLogSW = KadoMatsuInfo
 
 def createDataMart(
 	*dmargs: str,
@@ -50,57 +44,62 @@ def createDataMart(
 	useItem: str = ...,
 	csv_type: str = "CSV1S",
 	csv_code: str = "SHIFT_JIS"):
-
-	global __km
-	dm = __km.createDataMart()
-	print(">>KadoMatsu:createDataMart")
-
+	global __kmIns
+	global __kmLogSW
+	dm = __kmIns.createDataMart()
+	if __kmLogSW:
+		print(">>KadoMatsu:createDataMart")
 #	if useItem:
-#		print(">>KadoMatsu:DM:useItem ", useItem)
+#		if __kmLogSW:
+#			print(">>KadoMatsu:DM:useItem ", useItem)
 #		dm.useItem(useItem)
-
 	if dmargs:
 		if dmargs[0]:
 			csv = dmargs[0]
-
-	print(">>KadoMatsu:DM:readCSV ", csv)
+	if __kmLogSW:
+		print(">>KadoMatsu:DM:readCSV ", csv)
 	dm.readCSV(csv, csv_type, csv_code)
-	print(">>KadoMatsu:DM:readCSV Finish")
-
-	kmDm = kmDM(dm);
-	return kmDm;
+	if __kmLogSW:
+		print(">>KadoMatsu:DM:readCSV Finish")
+	kmDm = kmDM(__kmLogSW, dm)
+	return kmDm
 
 class kmDM:
-
+	_kmLogSW = False
 	_DM = None
-
-	def __init__(self, dm):
+	def __init__(self, kmLogSW, dm):
+		self._kmLogSW = kmLogSW
 		self._DM = dm
 
 	def select(self, *args: str):
-		print(">>KadoMatsu:DM:select ", args)
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:select ", args)
 		dm = self._DM.select(args)
-		print(">>KadoMatsu:DM:select Finish")
-		kmDm = kmDM(dm);
-		return kmDm;
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:select Finish")
+		return kmDM(self._kmLogSW, dm)
 
 	def where(self, args: str):
-		print(">>KadoMatsu:DM:where ", args)
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:where ", args)
 		dm = self._DM.where(args)
-		print(">>KadoMatsu:DM:where Finish")
-		kmDm = kmDM(dm);
-		return kmDm;
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:where Finish")
+		return kmDM(self._kmLogSW, dm)
 
 	def groupBy(self, *args: str):
-		print(">>KadoMatsu:DM:groupBy ", args)
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:groupBy ", args)
 		dm = self._DM.groupBy(args)
-		print(">>KadoMatsu:DM:groupBy Finish")
-		kmDm = kmDM(dm);
-		return kmDm;
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:groupBy Finish")
+		return kmDM(self._kmLogSW, dm)
 
 	def toCSV(self, *csv: str, 
 			csv_type: str = "CSV1S",
 			csv_code: str = "SHIFT_JIS"):
-		print(">>KadoMatsu:DM:toCSV ", csv[0])
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:toCSV ", csv[0])
 		self._DM.toCSV(csv[0], csv_type, csv_code)
-		print(">>KadoMatsu:DM:toCSV Finish")
+		if self._kmLogSW:
+			print(">>KadoMatsu:DM:toCSV Finish")
